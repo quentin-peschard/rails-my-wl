7efe7b37aaca627c9b2593757434d6e6d202f89e
Quentin Peschard <qpeschard49@gmail.com>
2021-09-25 16:23:24 +0200

    added relation between models

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,35 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
   end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
+
 end
