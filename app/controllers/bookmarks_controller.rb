2af120c3e90b16a9e92d06edd5e33689dff3ea32
Quentin Peschard <qpeschard49@gmail.com>
2021-09-27 11:45:41 +0200

    bookmark form working

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,6 +3,7 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
+    @bookmark.movie = Movie.find(params[:bookmark][:movie_id])
     if @bookmark.save
       redirect_to list_path(@list)
     else
