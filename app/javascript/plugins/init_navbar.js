5f9ed78d0d875633c9352b431b700712ace5027d
Quentin Peschard <qpeschard49@gmail.com>
2021-10-02 12:00:01 +0200

    done with home css for now

---
+++ app/javascript/plugins/init_navbar.js
@@ -0,0 +1,15 @@
+const changeNav = () => {
+  console.log('io')
+  const navbar = document.querySelector('.navbar');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-lewagon-white');
+      } else {
+        navbar.classList.remove('navbar-lewagon-white');
+      }
+    });
+  }
+}
+
+export { changeNav };
