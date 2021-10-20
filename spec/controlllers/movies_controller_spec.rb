f536ea2ea6db408a53488eec292ab04501e5367b
Quentin Peschard <qpeschard49@gmail.com>
2021-10-20 14:06:32 +0200

    movie controller index test

--- spec/controlllers/movies_controller_spec.rb
+++ spec/controlllers/movies_controller_spec.rb
@@ -2,5 +2,9 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
   describe 'GET index' do
+    it 'displays all the movies' do
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
 end
